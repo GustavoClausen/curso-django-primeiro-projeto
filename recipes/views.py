56d6746007b701918a72780e2151c4e505352af8
GustavoClausen <guguclausen@gmail.com>
2023-02-20 00:16:52 -0300

    Criado partial e header. Feito o  include na home.

--- recipes/views.py
+++ recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def root_page(request):
-    return render(request, 'recipes/home.html', {
+    return render(request, 'recipes/pages/home.html', {
         'name': 'Gustavo',
         'lastname': 'Clausen'
     })
