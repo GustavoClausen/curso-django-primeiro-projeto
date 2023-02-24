aa29637a96c37e0d507b1ff47317e640bb041df9
GustavoClausen <guguclausen@gmail.com>
2023-02-24 00:13:17 -0300

    Injetando dados na lista da view root_page

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 def root_page(request):
-    return render(request, 'recipes/pages/home.html', {
-        'name': 'Gustavo',
-        'lastname': 'Clausen'
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html')
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+    })
