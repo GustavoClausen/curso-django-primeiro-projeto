10387c82d5f6ee3506f535e693ea602125345917
GustavoClausen <guguclausen@gmail.com>
2023-03-10 22:37:23 -0300

    Ocultando receitas não publicadas com filter

--- recipes/urls.py
+++ recipes/urls.py
@@ -7,4 +7,6 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.root_page, name='home'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
 ]
