eea707174659838ed98801deaba36ad24cf1b084
GustavoClausen <guguclausen@gmail.com>
2023-04-01 11:52:39 -0300

    Testando valores default

--- recipes/tests/test_recipe_models.py
+++ recipes/tests/test_recipe_models.py
@@ -1,13 +1,31 @@
 from django.core.exceptions import ValidationError
 from parameterized import parameterized
 
-from .test_recipe_base import RecipeTestBase
+from .test_recipe_base import Recipe, RecipeTestBase
 
 
 class RecipeModelTest(RecipeTestBase):
     def setUp(self) -> None:
         self.recipe = self.make_recipe()
         return super().setUp()
+
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Test Defalt Category'),
+            author=self.make_author(username='NewUser'),
+            title='Recipe title',
+            description='Recipe description',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe preparation steps',
+            cover='recipes/covers/2023/03/08/pexels-team-picsfast-8753672.jpg',
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
 
     @parameterized.expand([
         ('title', 100),
@@ -19,3 +37,21 @@ class RecipeModelTest(RecipeTestBase):
         setattr(self.recipe, field, 'a' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_if_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg="Recipe preparation_steps_is_html isn't False"
+        )
+
+    def test_if_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        recipe.full_clean()
+        recipe.save()
+        self.assertFalse(
+            recipe.is_published,
+            msg="Recipe is_published isn't False"
+        )
