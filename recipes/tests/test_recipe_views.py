2a098b101dc634219c9cee30ea6b4f95d0ac91c5
GustavoClausen <guguclausen@gmail.com>
2023-04-04 22:56:59 -0300

    Corrigindo testes quebrados apos novos testes cria

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -152,6 +152,11 @@ class RecipeViewsTest(RecipeTestBase):
         self.assertIs(resolved.func, views.search)
 
     def test_recipe_search_loads_correct_template(self):
-        url = reverse('recipes:search')
+        url = reverse('recipes:search') + '?s=teste'
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raiseis_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
@@ -41,4 +42,8 @@ def category(request, category_id):
 
 
 def search(request):
+    search_term = request.GET.get('s')
+
+    if not search_term:
+        raise Http404()
     return render(request, 'recipes/pages/search.html')
