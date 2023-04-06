d9a9fcd03e1a8fe73c9ee5f0528416bc52fb7b4a
GustavoClausen <guguclausen@gmail.com>
2023-04-05 21:58:31 -0300

    Testando search no título e descrição

--- recipes/tests/test_recipe_search_view.py
+++ recipes/tests/test_recipe_search_view.py
@@ -29,3 +29,53 @@ class RecipeSearchViewTest(RecipeTestBase):
             'Pesquisa por &quot;&lt;teste&gt;&quot;',
             response.content.decode('utf-8')
         )
+
+    def test_recipe_search_can_find_recipe_by_title(self):
+        title1 = 'This is a recipe one'
+        title2 = 'This is a recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='one', title=title1, author_data={'username': 'one'}
+        )
+        recipe2 = self.make_recipe(
+            slug='two', title=title2, author_data={'username': 'two'}
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?s={title1}')
+        response2 = self.client.get(f'{search_url}?s={title2}')
+        response_both = self.client.get(f'{search_url}?s=this')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
+
+    def test_recipe_search_can_find_recipe_by_description(self):
+        desc1 = 'Description of recipe one'
+        desc2 = 'Description of recipe two'
+
+        recipe1 = self.make_recipe(
+            slug='one', description=desc1, author_data={'username': 'one'}
+        )
+        recipe2 = self.make_recipe(
+            slug='two', description=desc2, author_data={'username': 'two'}
+        )
+
+        search_url = reverse('recipes:search')
+        response1 = self.client.get(f'{search_url}?s={desc1}')
+        response2 = self.client.get(f'{search_url}?s={desc2}')
+        response_both = self.client.get(f'{search_url}?s=Description of')
+
+        self.assertIn(recipe1, response1.context['recipes'])
+        self.assertNotIn(recipe2, response1.context['recipes'])
+
+        self.assertIn(recipe2, response2.context['recipes'])
+        self.assertNotIn(recipe1, response2.context['recipes'])
+
+        self.assertIn(recipe1, response_both.context['recipes'])
+        self.assertIn(recipe2, response_both.context['recipes'])
