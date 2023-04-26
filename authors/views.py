1d885b3a5bfee8dded6b311ceb9292271120d917
GustavoClausen <guguclausen@gmail.com>
2023-04-25 22:55:58 -0300

    Criando views de login e logout

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,11 @@
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
 def register_view(request):
@@ -12,7 +14,7 @@ def register_view(request):
 
     return render(request, 'author/pages/register_view.html', {
         'form': form,
-        'form_action': reverse('authors:create'),
+        'form_action': reverse('authors:register_create'),
     })
 
 
@@ -31,5 +33,52 @@ def register_create(request):
         messages.success(request, 'Usuário criado com sucesso.')
 
         del (request.session['register_form_data'])
+        return redirect('authors:login')
 
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'author/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    if not request.POST:
+        raise Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', ''),
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Login realizado com sucesso.')
+            login(request, authenticated_user)
+
+        else:
+            messages.error(request, 'Usuário ou senha inválidos.')
+
+    else:
+        messages.error(request, 'Erro na validação do formulário.')
+
+    return redirect(login_url)
+
+
+@login_required(login_url='authors:login', redirect_field_name='next')
+def logout_view(request):
+    if not request.POST:
+        return redirect(reverse('authors:login'))
+
+    if request.POST.get('username') != request.user.username:
+        return redirect(reverse('authors:login'))
+
+    logout(request)
+    return redirect(reverse('authors:login'))
---
+++ utils/django_forms.py
@@ -0,0 +1,24 @@
+import re
+
+from django.core.exceptions import ValidationError
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_value):
+    add_attr(field, 'placeholder', placeholder_value)
+
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError(
+            ('A senha precisa ter letras maísculas, minúsculas e números.'),
+            code='invalid'
+        )
+
+    return
