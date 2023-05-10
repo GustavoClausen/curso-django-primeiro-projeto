2fecd93b5b42719ddcad9dc29437f0035632d6b4
GustavoClausen <guguclausen@gmail.com>
2023-05-09 22:56:49 -0300

    Ajustando administração padrão

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Category, Recipe
 
 
@@ -8,7 +9,15 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'author', 'is_published', 'created_at', 'id')
+    list_display_links = ['title']
+    search_fields = 'id', 'title', 'description', 'slug', 'preparation_steps'
+    list_filter = [
+        'category', 'author', 'is_published', 'preparation_steps_is_html'
+    ]
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = '-id',
 
 
 admin.site.register(Category, CategoryAdmin)
