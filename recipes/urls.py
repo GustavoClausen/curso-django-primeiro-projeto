e142553fcd1fc1dba02fc34aec1f161d63ab6bb2
GustavoClausen <guguclausen@gmail.com>
2023-05-30 22:50:46 -0300

    Json response

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,12 +5,34 @@ from . import views
 app_name = 'recipes'
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name='home'),
-    path('recipes/search/',
-         views.RecipeListViewSearch.as_view(), name='search'
-         ),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
-    path('recipes/category/<int:category_id>/',
-         views.RecipeListViewCategory.as_view(), name='category'),
-
+    path(
+        '',
+        views.RecipeListViewHome.as_view(),
+        name='home'
+    ),
+    path(
+        'recipes/search/',
+        views.RecipeListViewSearch.as_view(),
+        name='search'
+    ),
+    path(
+        'recipes/<int:pk>/',
+        views.RecipeDetail.as_view(),
+        name='recipe'
+    ),
+    path(
+        'recipes/api/<int:pk>/',
+        views.RecipeDetailAPI.as_view(),
+        name='recipe_detail_api'
+    ),
+    path(
+        'recipes/api/',
+        views.RecipeListViewHomeAPI.as_view(),
+        name='recipes_api'
+    ),
+    path(
+        'recipes/category/<int:category_id>/',
+        views.RecipeListViewCategory.as_view(),
+        name='category'
+    ),
 ]
