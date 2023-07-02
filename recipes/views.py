6982a1d84273d792f0079afdc7db26ffff1b5fdb
GustavoClausen <guguclausen@gmail.com>
2023-07-01 21:09:53 -0300

    Adicionando pyteste e atualizando chromedriver

--- recipes/views.py
+++ recipes/views.py
@@ -10,6 +10,7 @@ from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
 from recipes.models import Recipe
+from tag.models import Tag
 from utils.pagination import make_pagination
 
 ITEMS_PER_PAGE = int(os.environ.get('ITEMS_PER_PAGE', 6))
@@ -59,6 +60,7 @@ class RecipeListViewBase(ListView):
         qs = super().get_queryset(*args, **kwargs)
         qs = qs.filter(is_published=True)
         qs = qs.select_related('author', 'category')
+        qs = qs.prefetch_related('tags')
         return qs
 
     def get_context_data(self, *args, **kwargs):
@@ -187,3 +189,28 @@ class RecipeDetailAPI(RecipeDetail):
             recipe_dict,
             safe=False,
         )
+
+
+class RecipeListViewTag(RecipeListViewBase):
+    template_name = 'recipes/pages/tag.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(tags__slug=self.kwargs.get('slug', ''))
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_title = Tag.objects \
+            .filter(slug=self.kwargs.get('slug', '')).first()
+
+        if not page_title:
+            page_title = 'No recipes found'
+
+        page_title = page_title
+        ctx.update(
+            {
+                'page_title': page_title,
+            }
+        )
+        return ctx
