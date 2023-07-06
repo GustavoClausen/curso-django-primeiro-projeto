009c309a18ca55ea97ef22090e4570445c0e0443
GustavoClausen <guguclausen@gmail.com>
2023-07-05 23:53:14 -0300

    Adicionando JS na página

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -1,6 +1,5 @@
 function my_scope() {
     const forms = document.querySelectorAll('.form-delete')
-
     for (const form of forms) {
         form.addEventListener('submit', function(e){
             e.preventDefault();
@@ -12,6 +11,36 @@ function my_scope() {
             }
            }) 
     }
+
+    (
+        () => {
+           const btnCloseMenu = document.querySelector('.btn-close-menu');
+           const btnShowMenu = document.querySelector('.btn-show-menu');
+           const menuContainer = document.querySelector('.menu-container');
+           const btnShowMenuVisible = 'btn-show-menu-visible';
+           const menuHiddenClass = 'menu-hidden';
+           const closeMenu = () => {
+
+            btnShowMenu.classList.add(btnShowMenuVisible);
+            menuContainer.classList.add(menuHiddenClass);
+            };
+           const showMenu = () => {
+                btnShowMenu.classList.remove(btnShowMenuVisible);
+                menuContainer.classList.remove(menuHiddenClass);
+           };
+           
+
+           if(btnCloseMenu) {
+            btnCloseMenu.removeEventListener('click', closeMenu);
+            btnCloseMenu.addEventListener('click', closeMenu);
+           }
+
+           if(btnShowMenu) {
+            btnShowMenu.removeEventListener('click', showMenu);
+            btnShowMenu.addEventListener('click', showMenu);
+           }
+        }
+    )();
 }
 
 my_scope()
