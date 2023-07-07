4aceacda69ec002aa01e5d2a034f36494d00108a
GustavoClausen <guguclausen@gmail.com>
2023-07-06 21:17:12 -0300

    Criando form (post) de logout

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -41,6 +41,19 @@ function my_scope() {
            }
         }
     )();
-}
 
+    (() => {
+        const authorLogoutLinks = document.querySelectorAll('.authors-logout-link');
+        const formLogout = document.querySelector('.form-logout');
+    
+        for(const link of authorLogoutLinks) {
+            link.addEventListener('click', (e) => {
+                e.preventDefault();
+                formLogout.submit();
+            });
+        }
+    }
+    )();
+}
 my_scope()
+
