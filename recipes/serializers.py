379810d7c6549c82da8c9963c6b0c76230ee1d3b
GustavoClausen <guguclausen@gmail.com>
2023-08-01 21:33:45 -0300

    Relacionamento de campos de tabela

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,7 +1,42 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+
+from tag.models import Tag
+
+from .models import Category
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=155)
+    slug = serializers.SlugField(max_length=155)
 
 
 class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=155)
     description = serializers.CharField(max_length=200)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField(
+        method_name='any_method_name')
+
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+    )
+    category_name = serializers.StringRelatedField(
+        source='category'
+    )
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(),
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        many=True,
+        source='tags',
+    )
+
+    def any_method_name(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
